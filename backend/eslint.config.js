eab4a517bc559f0d568010fc6dab06e86b82702d
Akimana A Jean d Amour <akimanaja17@gmail.com>
2026-02-24 12:47:59 +0200

    Fix configurations

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -2,22 +2,26 @@ import js from '@eslint/js';
 import eslintConfigPrettier from 'eslint-config-prettier';
 import tseslint from 'typescript-eslint';
 
+const tsTypeCheckedConfigs = tseslint.configs.recommendedTypeChecked.map((config) => ({
+  ...config,
+  files: ['**/*.ts'],
+}));
+
 export default [
   {
     ignores: ['dist/**', 'node_modules/**', 'coverage/**'],
   },
   js.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
   eslintConfigPrettier,
+  ...tsTypeCheckedConfigs,
   {
+    files: ['**/*.ts'],
     languageOptions: {
       parserOptions: {
-        project: true,
+        project: ['./tsconfig.json'],
         tsconfigRootDir: import.meta.dirname,
       },
     },
-  },
-  {
     rules: {
       '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
     },
