d3bfeb84463b064f319904c8f64e1d25ba6c40f5
ABIRAGIYE Obed <obedabiragiye@gmail.com>
2026-04-24 11:42:48 +0200

    Implementing currency  types and  Service to create  new currency, enable or disable base currency.

---
+++ backend/api/currencies/services.ts
@@ -0,0 +1,73 @@
+import { BaseService } from "../../services/base.service";
+import type { Currency } from "../types/currency";
+
+export class CurrencyService extends BaseService<Currency> {
+  constructor() {
+    super('currencies')
+  }
+
+  async CreateCurrency(data: Omit<Currency, 'id'>): Promise<Currency>{
+
+    const existing = await this.findOne({ code: data.code });
+
+    if(existing) {
+      throw new Error('Currency already exists');
+    }
+
+    if (data.isBase) {
+      await this.clearBaseCurrency();
+      data.exchangeRate = 1;
+    }
+
+    return this.create({
+      ...data,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
+  }
+  async enableBaseCurrency(id: number): Promise<boolean> {
+    const currency = await this.findById(id);
+
+    if (!currency) {
+      throw new Error('Currency not found');
+    }
+
+    await this.clearBaseCurrency();
+   
+    return this.update(id, {
+    isBase: true,
+    exchangeRate: 1,
+    updatedAt: new Date(),
+   });
+  }
+
+  async disableBaseCurrency(id: number): Promise<boolean> {
+    const currency = await this.findById(id);
+
+    if(!currency) {
+      throw new Error('Currency is not found');
+    }
+
+    if(!currency.isBase) {
+      throw new Error('This currecy is not base');
+    }
+
+    return this.update(id, {
+      isBase: false,
+      updatedAt: new Date(),
+    });
+  }
+  
+  private async clearBaseCurrency(): Promise<void> {
+    await this.pool.query(`
+      UPDATE currencies
+      SET is_base = false
+      WHERE is_base = true;
+    `);
+  }
+}
+
+
+
+  
+  
---
+++ backend/api/types/currency.ts
@@ -0,0 +1,10 @@
+export interface Currency {
+  id : number;
+  code: string;
+  name: string;
+  symbol: string;
+  isBase: boolean;
+  exchangeRate: number;
+  createdAt: Date;
+  updatedAt: Date;
+}
